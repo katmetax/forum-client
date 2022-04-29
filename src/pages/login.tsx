2bf34d9757dce13315b50a926ee2004551fa1057
Katerina Metaxopoulou <kat@katmetax.com>
2022-04-29 08:04:48 +0100

    Add graphql mutations and login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,24 +4,24 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import InputField from '../components/InputField';
 import Wrapper from '../components/Wrapper';
-import { useRegisterMutation } from '../generated/graphql';
+import { useLoginMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/errorMap';
 
-interface RegisterProps {}
+interface LoginProps {}
 
-const Register: React.FC<RegisterProps> = () => {
+const Login: React.FC<LoginProps> = () => {
 	const router = useRouter();
-	const [, register] = useRegisterMutation();
+	const [, login] = useLoginMutation();
 	return (
 		<Wrapper>
 			<Formik
 				initialValues={{ username: '', password: '' }}
 				onSubmit={async (values, { setErrors }) => {
-					const response = await register(values);
+					const response = await login({ options: values });
 
-					if (response.data?.register.errors) {
-						setErrors(toErrorMap(response.data.register.errors));
-					} else if (response.data?.register.user) {
+					if (response.data?.login.errors) {
+						setErrors(toErrorMap(response.data.login.errors));
+					} else if (response.data?.login.user) {
 						router.push('/');
 					}
 				}}
@@ -37,7 +37,7 @@ const Register: React.FC<RegisterProps> = () => {
 							isLoading={isSubmitting}
 							type='submit'
 						>
-							Register
+							Login
 						</Button>
 					</Form>
 				)}
@@ -46,4 +46,4 @@ const Register: React.FC<RegisterProps> = () => {
 	);
 };
 
-export default Register;
+export default Login;
